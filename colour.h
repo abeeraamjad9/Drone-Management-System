4f49d839a2c1a41188926f7efdbd58d28a7f0042
rabia-tech-ops <ozairrabia@gmail.com>
2026-05-14 10:08:35 +0500

    colour.h

---
+++ colour.h
@@ -0,0 +1,18 @@
+#ifndef COLOR_H
+#define COLOR_H
+
+// Text Colors
+#define RESET   "\033[0m"
+#define RED     "\033[31m"
+#define GREEN   "\033[32m"
+#define YELLOW  "\033[33m"
+#define BLUE    "\033[34m"
+#define MAGENTA "\033[35m"
+#define CYAN    "\033[36m"
+#define WHITE   "\033[37m"
+// Bold Colors
+#define BOLD_RED     "\033[1;31m"
+#define BOLD_GREEN   "\033[1;32m"
+#define BOLD_BLUE    "\033[1;34m"
+
+#endif
